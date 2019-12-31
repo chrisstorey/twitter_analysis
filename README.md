95c46b7f16464aa0a94d3d334359b16c79c5c3ff
chrisstorey <chris.storey@working-age.digital.dwp.gov.uk>
2019-12-31 15:13:01 +0000

    .

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 Based on work from :
 https://towardsdatascience.com/creating-the-twitter-sentiment-analysis-program-in-python-with-naive-bayes-classification-672e5589a7ed
 
+and
+
+https://www.geeksforgeeks.org/twitter-sentiment-analysis-using-python/
--- twitter_analysis.py
+++ twitter_analysis.py
@@ -7,7 +7,19 @@ twitter_api = twitter.Api(consumer_key=TWITTER_CONSUMER_KEY,
                           access_token_key=TWITTER_OAUTH_TOKEN,
                           access_token_secret=TWITTER_OAUTH_SECRET)
 
-# test authentication
-print(twitter_api.VerifyCredentials())
 
+def buildTestSet(search_keyword):
+    try:
+        tweets_fetched = twitter_api.GetSearch(search_keyword, count = 100)
 
+        print("Fetched " + str(len(tweets_fetched)) + " tweets for the term " + search_keyword)
+
+        return [{"text":status.text, "label":None} for status in tweets_fetched]
+    except:
+        print("Unfortunately, something went wrong..")
+        return None
+
+search_term = input("Enter a search keyword:")
+testDataSet = buildTestSet(search_term)
+
+print(testDataSet[0:4])
